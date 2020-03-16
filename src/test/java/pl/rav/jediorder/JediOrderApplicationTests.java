be87aa406845d148037f0079ba85d536f155eee4
Rafal Sokolowski <Rafal.Miroslaw.Sokolowski@gmail.com>
2020-03-16 15:46:16 +0100

    project setup for SpringBoot withThymeleaf

---
+++ src/test/java/pl/rav/jediorder/JediOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rav.jediorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JediOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
